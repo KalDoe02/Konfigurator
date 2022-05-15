e8d04959267e1c07d98921d3edb61190128fc993
Felix <felree16@student.fh-westkueste.de>
2022-05-15 12:12:26 +0200

    Controller Klasse und Controller_Selector_CSV Klasse hinzugefügt

---
+++ CrazyCarKonfigurator/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyCarKonfigurator
+{
+    internal class Controller
+    {
+        public string Bezeichnung { get; set; }
+        public float Preis { get; set; }
+        public int Gewicht { get; set; }
+        public int Taktrate { get; set; }
+        public int PinAnzahl { get; set; }
+        public bool UART { get; set; }
+        public bool I2C { get; set; }
+
+      
+
+    }
+}
---
+++ CrazyCarKonfigurator/Controller_Selector_CSV.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CrazyCarKonfigurator
+{
+    internal class Controller_Selector_CSV
+    {
+        private List<Controller> controllers;
+
+        public Controller_Selector_CSV()
+        {
+            controllers = new List<Controller>();
+
+            string[] zeilen = File.ReadAllLines(@"\Database\Database Controller.csv");
+            foreach (string zeile in zeilen )
+            {
+                string[] daten = zeile.Split(';');
+                string Bezeichnung = daten[1];
+                float Preis = float.Parse(daten[2]);
+                int Gewicht = int.Parse(daten[3]);
+                int Taktrate = int.Parse(daten[4]);
+                int PinAnzahl = int.Parse(daten[5]);
+                bool UART = bool.Parse(daten[6]);
+                bool I2C = bool.Parse(daten[7]);
+
+                controllers.Add(new Controller { Bezeichnung = Bezeichnung, Preis = Preis, Gewicht = Gewicht, Taktrate = Taktrate, PinAnzahl = PinAnzahl, UART = UART, I2C = I2C });
+            }
+        }
+
+        public Controller[] getAll()
+        {
+            return controllers.ToArray();
+        }
+
+
+    }
+}

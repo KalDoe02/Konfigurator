1b54121fd4975ad57520189831e071a37cba1a74
otchix <otchix@gmail.com>
2022-05-31 16:41:18 +0200

    Motor

---
+++ Klassenbibliothek/Motor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassenbibliothek
+{
+    public class Motor
+    {
+        public string Bezeichnung { get; set; }
+        public float Preis { get; set; }
+        public int Gewicht { get; set; }
+        public double Nenndrehzahl { get; set; }
+        public float Stromaufnahme { get; set; }
+
+
+
+    }
+}
